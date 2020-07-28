494f760c544f44101b0c4473d63c6e8d7d4687c5
aharon050 <aharontadevosyan1996@gmail.com>
2020-07-29 02:56:51 +0400

    'update'

--- src/store/reducers/singUpReducer.js
+++ src/store/reducers/singUpReducer.js
@@ -15,71 +15,7 @@ const singUpReducer = (state = initialState, action) => {
 
 export default singUpReducer;
 
-export const data = [
-  {
-    id: 1,
-    name: "Gagp",
-    topic: "Hooks",
-    project: "Canvas",
-    members: [
-      {
-        firstName: "brat",
-        lastName: "Lname",
-        avatarUrl:
-          "https://sun9-57.userapi.com/c830508/v830508655/a6b55/Jc7lYIlpOZk.jpg",
-      },
-      {
-        firstName: "bratan",
-        lastName: "Lname",
-        avatarUrl:
-          "https://sun9-57.userapi.com/c830508/v830508655/a6b55/Jc7lYIlpOZk.jpg",
-      },
-      {
-        firstName: "bratishka",
-        lastName: "Lname",
-        avatarUrl:
-          "https://sun9-57.userapi.com/c830508/v830508655/a6b55/Jc7lYIlpOZk.jpg",
-      },
-      {
-        firstName: "jigyar",
-        lastName: "Lname",
-        avatarUrl:
-          "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-760w.JPG",
-      },
-    ],
-  },
 
 
-  {
-    id: 1,
-    name: "Gagp",
-    topic: "JS",
-    project: "React",
-    members: [
-      {
-        firstName: "brat",
-        lastName: "Lname",
-        avatarUrl:
-            "https://sun9-57.userapi.com/c830508/v830508655/a6b55/Jc7lYIlpOZk.jpg",
-      },
-      {
-        firstName: "bratan",
-        lastName: "Lname",
-        avatarUrl:
-            "https://sun9-57.userapi.com/c830508/v830508655/a6b55/Jc7lYIlpOZk.jpg",
-      },
-      {
-        firstName: "bratishka",
-        lastName: "Lname",
-        avatarUrl:
-            "https://sun9-57.userapi.com/c830508/v830508655/a6b55/Jc7lYIlpOZk.jpg",
-      },
-      {
-        firstName: "jigyar",
-        lastName: "Lname",
-        avatarUrl:
-            "https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-760w.JPG",
-      },
-    ],
-  },
-];
+
+
